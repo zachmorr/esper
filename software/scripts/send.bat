70e5879d54d925e696861821034fea2fb481fbc8
Morris <zachary.morris@spectrumcontrols.com>
2021-04-05 13:31:25 -0700

    Added build scripts for Windows

---
+++ software/scripts/send.bat
@@ -0,0 +1,1 @@
+scp %~dp0..\firmware\build\*.bin %~dp0..\firmware\build\bootloader\bootloader.bin %~dp0..\firmware\build\ota_data_initial.bin %~dp0..\firmware\build\partition_table\partition-table.bin %1;
